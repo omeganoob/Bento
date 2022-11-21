639719b91a3f1a080092793d9ba3699ede5c0304
omeganoob <nguyenchung666644136@gmail.com>
2022-11-22 00:46:05 +0700

    Update greeting.js

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -13,11 +13,11 @@ const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 if (hour >= 23 || hour < 5) {
-	document.getElementById('greetings').innerHTML = `{gree1} + <span style="color: #C4E538">{name}</span>`;
+	document.getElementById('greetings').innerHTML = `${gree1}<span style="color: #C4E538">${name}</span>`;
 } else if (hour >= 6 && hour < 12) {
-	document.getElementById('greetings').innerHTML = `{gree2} + <span style="color: #C4E538">{name}</span>`;
+	document.getElementById('greetings').innerHTML = `${gree2}<span style="color: #C4E538">${name}</span>`;
 } else if (hour >= 12 && hour < 17) {
-	document.getElementById('greetings').innerHTML = `{gree3} + <span style="color: #C4E538">{name}</span>`;
+	document.getElementById('greetings').innerHTML = `${gree3}<span style="color: #C4E538">${name}</span>`;
 } else {
-	document.getElementById('greetings').innerHTML = `{gree4} + <span style="color: #C4E538">{name}</span>`;
+	document.getElementById('greetings').innerHTML = `${gree4}<span style="color: #C4E538">${name}</span>`;
 }
